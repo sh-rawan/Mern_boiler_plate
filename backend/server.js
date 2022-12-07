49fe9f4262fd7122225c6d51ab34bbc17d0e12a7
Shrawan Agrawal <agrawalshrawan245@gmail.com>
2022-12-08 00:04:43 +0900

    Final deploy

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,22 @@ const connectDB = require("./database/db.js")
 const app = express()
 
 dotenv.config()
-// connectDB()
-
-app.use(express.json())
-
-app.get("/", (req,res) => { res.send("API is running!!") })
-
+const PORT = 5000 || process.env.PORT
 const dirname = path.resolve()
+app.use(express.json())
+// connectDB()
 
 readdirSync("./routes").map((r)=>app.use("/api", require(`./routes/${r}`)))
 
-app.listen(process.env.PORT, console.log(`Server is listening in ${process.env.NODE_ENV} environment on port ${process.env.PORT}!!`.rainbow))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(dirname, '/../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(dirname, "..", 'frontend', 'build', 'index.html')))
+
+} else app.get('/', (req, res) => {res.send('API is running....')})
+
+
+app.listen(PORT, console.log(`Server is listening in ${process.env.NODE_ENV} environment on port ${PORT}!!`.rainbow))
 
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Pages } from "./pages/Pages";
 
 
 function App() {
   return (
+  <HashRouter>
   <Router>
     <Route path="/" component={Home} exact />
     <Route path="/pages" component={Pages} exact />
   </Router>
+  </HashRouter>
 );
 }
 
